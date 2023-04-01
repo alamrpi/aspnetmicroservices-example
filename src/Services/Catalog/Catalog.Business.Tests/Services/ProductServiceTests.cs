0466db8cc0d7c8b7b7a85afb0acc909c41803a49
Md Alam Hossain <alam.rpi@hotmail.com>
2023-04-01 11:39:50 +0600

    Test Implemented

---
+++ src/Services/Catalog/Catalog.Business.Tests/Services/ProductServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Business.IServices;
+using Catalog.Data.Context;
+using Catalog.Entities;
+using Moq;
+
+namespace Catalog.Business.Tests.Services
+{
+    public class ProductServiceTests
+    {
+        private Mock<ICatalogContext> _catalogContext;
+        public ProductServiceTests()
+        {
+            _catalogContext = new Mock<ICatalogContext>();
+        }
+
+        [Fact]
+        public async void GetProducts_ReturnAllProducts()
+        {
+            //Arrange 
+            var products = new List<Product>() { 
+                new Product
+                {
+                    Id = "11111111",
+                    Category = "Category One",
+                    Name = "Name here",
+                    Price = 200,
+                    Description = "Description here",
+                    Summary = "Summery Here",
+                    ImageFile = "default.png"
+                }
+            };
+            Mock<IProductService> _productService = new Mock<IProductService>();
+            _productService.Setup(x => x.GetProducts()).ReturnsAsync(products.AsEnumerable());
+
+            var result = await _productService.Object.GetProducts();
+
+            Assert.Equal(products, result);
+
+        }
+    }
+}
--- src/Services/Catalog/Catalog.Business/Services/ProductService.cs
+++ src/Services/Catalog/Catalog.Business/Services/ProductService.cs
@@ -2,11 +2,6 @@
 using Catalog.Data.Context;
 using Catalog.Entities;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Catalog.Business.Services
 {
